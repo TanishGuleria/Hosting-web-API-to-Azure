e35e367871598c45172cda52be7d257e99e9c772
tanish guleria <tanishguleria47@gmail.com>
2020-05-02 14:56:21 +0530

    Add project files.

---
+++ Controllers/LaptopController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Laptop.Models;
+namespace Laptop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LaptopController : ControllerBase
+    {
+        // GET: api/Laptop
+
+
+       
+        [HttpGet]
+        public laptop Get()
+        {
+            Models.laptop l = new Models.laptop()
+            {
+                Id = 1,
+                Name = "dell"
+            };
+
+            return l;
+
+        }
+    }
+}
